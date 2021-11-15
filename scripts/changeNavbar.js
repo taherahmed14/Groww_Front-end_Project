6b7be3e646ebd936256ad63d729558796f1dcaa2
taherahmed14 <taherahmed.sj.95@gmail.com>
2021-11-15 11:55:39 +0530

    Log out option

--- scripts/changeNavbar.js
+++ scripts/changeNavbar.js
@@ -7,3 +7,26 @@ if(check.length > 0){
     loginBtn.setAttribute("class", "dontDisplay");
     userAccount.setAttribute("class", "afterLogin");
 }
+
+let user = document.getElementById("user");
+let logout = document.getElementById("logOutDropDown");
+let count = 0;
+
+user.onclick = () => {
+    if(count == 0){
+        logout.setAttribute("class", "displayDropDown");
+        logout.onclick = () => {
+            console.log("log out working");
+            check.pop();
+            localStorage.setItem("check_Login", JSON.stringify(check));
+            logout.setAttribute("class", "noDropDownDisplay");
+            location.reload();
+        }
+        count++;
+    }
+    else if(count == 1){
+        logout.setAttribute("class", "noDropDownDisplay");
+        count = 0;
+    }
+}
+
